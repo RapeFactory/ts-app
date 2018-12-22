6da9f70777bc207798b420cb9b9c2c2b323a5a14
Alexandr Makeev <admakeye@mts.ru>
2018-12-22 17:04:12 +0300

    render avatar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component, FormEvent, KeyboardEvent } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
 import View from './components/View';
 
-class App extends Component {
+interface IState {
+  username: string;
+  avatar_url: string;
+  html_url: string;
+}
+class App extends Component<any, IState> {
+  state = {
+    username: '',
+    avatar_url: '',
+    html_url: '',
+  };
+
+  async getUser(username: string) {
+    const URL = `https://api.github.com/users/${username}`;
+    await fetch(URL)
+      .then(data => data.json())
+      .then(({ avatar_url, html_url }) => this.setState({ avatar_url, html_url }))
+      .catch(console.error);
+  }
+
+  onKey = (e: KeyboardEvent<HTMLInputElement>): void => {
+    const { username } = this.state;
+    if (e.key === 'Enter' && username) {
+      this.getUser(username);
+    }
+  };
+
+  onInput = (e: FormEvent<HTMLInputElement>): void => {
+    if (e !== null) {
+      this.setState({ username: e.currentTarget.value });
+    }
+  };
+
   render() {
+    const { username, avatar_url, html_url } = this.state;
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
+          <img src={avatar_url || logo} className="App-logo" alt="logo" />
+          <input type="text" placeholder="КЛИККЛАК" onInput={this.onInput} onKeyDown={this.onKey} />
+          <a className="App-link" href={html_url || 'https://reactjs.org'} target="_blank" rel="noopener noreferrer">
+            {html_url !== '' ? username : 'Learn React'}
           </a>
           <Counter>{props => <View {...props} />}</Counter>
         </header>
