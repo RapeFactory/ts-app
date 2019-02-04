02bf6962987b47ccbcada1f769c8eb8f222bfea7
Alexandr Makeev <admakeye@mts.ru>
2019-02-04 10:12:16 +0300

    stash

--- src/components/View/index.tsx
+++ src/components/View/index.tsx
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import { RenderProps } from '../Counter';
 
 const View: FunctionComponent<RenderProps> = ({ increment, decrement, reset, count }) => (
-  <div>
+  <div className="Counter">
     <div>
       <h3>Render Props Counter</h3>
     </div>
     <div>
-      <p>{count}</p>
+      <div className="counter"><p>{count}</p></div>
       <button onClick={decrement}>Decrement</button>
       <button onClick={reset}>Reset</button>
       <button onClick={increment}>Increment</button>
